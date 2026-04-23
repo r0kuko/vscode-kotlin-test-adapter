b60258749ba4a3214d1a8f861895b8f652f11780
r0kuko <tianwen.liu@cloudstack-tech.com>
2026-04-24 00:21:39 +0800

    feat: Update Gradle and Kotlin configurations
    - Upgraded Kotlin JVM plugin from version 1.9.24 to 2.3.21 in build.gradle.kts.
    - Configured JVM toolchain to version 25 for all subprojects.
    - Added Gradle wrapper files including gradle-wrapper.jar and gradle-wrapper.properties for version 9.4.1.
    - Introduced gradlew and gradlew.bat scripts for easier Gradle command execution on Unix and Windows systems.

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,9 +1,17 @@
 plugins {
-    kotlin("jvm") version "1.9.24" apply false
+    kotlin("jvm") version "2.3.21" apply false
 }
 
 allprojects {
     repositories {
         mavenCentral()
+    }
+}
+
+subprojects {
+    plugins.withId("org.jetbrains.kotlin.jvm") {
+        extensions.configure<org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension> {
+            jvmToolchain(25)
+        }
     }
 }
